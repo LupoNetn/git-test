3784a7d1f36c5b5d3d5ff02b80fefbf5a2ec8362
oluwatobi <luponet07@gmail.com>
2025-08-24 16:52:57 -0400

    new files added

--- webpack-app/src/greeting.js
+++ webpack-app/src/greeting.js
@@ -1,1 +1,4 @@
+import './styles.css';
+
 export const greeting = "Hello, Odinite"
+
--- webpack-app/src/index.js
+++ webpack-app/src/index.js
@@ -1,3 +1,9 @@
+import image from './car-rental.jpg';
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+
+const imageView = document.createElement('img');
+imageView.src = image;
+
+document.body.appendChild(imageView);
